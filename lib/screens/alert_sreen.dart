18b25319a5b5e26bd9177be98cbbbc3c77c17c57
brayan <br4y47.r4m0s@gmail.com>
2022-10-14 16:04:17 -0400

    Terminamos la pantalla de la alerta

--- lib/screens/alert_sreen.dart
+++ lib/screens/alert_sreen.dart
@@ -1,8 +1,86 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
+
+
+  void displayDialogIOS(BuildContext context){
+
+    showCupertinoDialog(
+      barrierDismissible: true,
+      context: context, 
+      builder: (context){
+        return CupertinoAlertDialog(
+          title: const Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de la alerta'),
+              SizedBox(height: 10,),
+              FlutterLogo(size: 100,)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: const Text(
+                'Cancelar', 
+                style: TextStyle(
+                  color:Colors.red,
+                ),
+              )
+            ),
+            TextButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: const  Text('ok')
+            ),
+          ],
+        );
+      }
+    );
+
+  }
+
+  void displayDialogAndroid(BuildContext context){
+
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          elevation: 5,
+          title: const Text('Titulo'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de la alerta'),
+              SizedBox(height: 10,),
+              FlutterLogo(size: 100,)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: const  Text('Cancelar'))
+          ],
+        );
+      }
+    );
+
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -23,9 +101,10 @@ class AlertScreen extends StatelessWidget {
               ),
             ),
           ),
-          onPressed: (){
-
-          }, 
+          //onPressed: ()=>displayDialogAndroid(context)
+          onPressed: ()=> Platform.isAndroid
+            ? displayDialogAndroid(context)
+            : displayDialogIOS(context)
           
         )
       ),
